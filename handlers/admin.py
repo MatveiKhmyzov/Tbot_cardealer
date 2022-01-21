d3a4cbfe110daca236c291207e6eb4a48031f7ec
MatveiKhmyzov <matw.hmizov@yandex.ru>
2022-01-21 15:09:55 +0300

    add "the machine of states" and admin manage part with access rights verification

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,11 @@
-from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher import FSMContext, Dispatcher
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types
+from create_bot import dp, bot
+from aiogram.dispatcher.filters import Text
+
+
+ID = None
 
 
 class FSMAdmin(StatesGroup):
@@ -9,8 +14,81 @@ class FSMAdmin(StatesGroup):
     description = State()
     price = State()
 
+
+# Получение ID текущего модератора
+# @dp.message_handler(commands=['moderator'], is_chat_admin=True)
+async def make_changes_command(message: types.Message):
+    global ID
+    ID = message.from_user.id
+    await bot.send_message(message.from_user.id, 'Слушаю Вас...')
+    await message.delete()
+
+
 # Начало диалога загрузки нового пункта меню
-@dp.message_handler(commands='Загрузить', state=None)
-async def cm_start(message : types.Message):
-    await FSMAdmin.photo.set()
-    await message.reply('Загрузи фото')
+# @dp.message_handler(commands='Загрузить', state=None)
+async def cm_start(message: types.Message):
+    if message.from_user.id == ID:
+        await FSMAdmin.photo.set()
+        await message.reply('Загрузи фото')
+
+
+# Первый ответ и запись его в словарь
+# @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
+async def load_photo(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['photo'] = message.photo[0].file_id
+        await FSMAdmin.next()
+        await message.reply('Введите модель')
+
+
+# @dp.message_handler(state=FSMAdmin.name)
+async def load_name(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Введите комплектацию')
+
+
+# @dp.message_handler(state=FSMAdmin.description)
+async def load_description(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['description'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Введите цену')
+
+
+# @dp.message_handler(state=FSMAdmin.dprice)
+async def load_price(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = float(message.text)
+
+        async with state.proxy() as data:
+            await message.reply(str(data))
+        await state.finish()
+
+
+# Выход из машины состояний
+# @dp.message_handler(state='*', commands='Отмена')
+# @dp.message_handler(Text(equals='Отмена', ignore_case=True), state="*")
+async def cancel_handlers(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply("Изменения отменены")
+
+
+def register_handlers_admin(dp: Dispatcher):
+    dp.register_message_handler(cm_start, commands=['Загрузить'], state=None)
+    dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
+    dp.register_message_handler(load_name, state=FSMAdmin.name)
+    dp.register_message_handler(load_description, state=FSMAdmin.description)
+    dp.register_message_handler(load_price, state=FSMAdmin.price)
+    dp.register_message_handler(cancel_handlers, state='*', commands='Отмена')
+    dp.register_message_handler(cancel_handlers, Text(equals='Отмена', ignore_case=True), state='*')
+    dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
