06ebe39b6224a906dda86e6e2cfce2d5f91d4f9f
Matvei Khmyzov <matw.hmizov@yandex.ru>
2022-01-30 19:19:18 +0300

    add some blocks of try, except

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-from aiogram.utils import executor # для запуска бота в режиме long_polling (постоянный опрос сервера)
+from aiogram.utils import executor  # для запуска бота в режиме long_polling (постоянный опрос сервера)
 from create_bot import dp
 from handlers import client, admin, other
 from data_base import pgsql_db
+import os
 
 
 async def on_startup(_):
@@ -13,4 +14,6 @@ client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
+
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
--- data_base/pgsql_db.py
+++ data_base/pgsql_db.py
@@ -5,36 +5,51 @@ from create_bot import bot
 
 
 def pga_start():
-    global base, cur
-    base = ps.connect(user=user,
-                      password=password,
-                      host=host)
-    cur = base.cursor() # экземпляр для работы с содержимым базы
-    if base:
-        print('Database connected OK!')
-    cur.execute('CREATE TABLE IF NOT EXISTS cars(img TEXT, name TEXT PRIMARY KEY, description TEXT, price TEXT);')
-    base.commit()
+    try:
+        global base, cur
+        base = ps.connect(user=user,
+                          password=password,
+                          host=host)
+        cur = base.cursor()  # экземпляр для работы с содержимым базы
+        if base:
+            print('Database connected OK!')
+        cur.execute('CREATE TABLE IF NOT EXISTS cars(img TEXT, name TEXT PRIMARY KEY, description TEXT, price TEXT);')
+        base.commit()
+    except (Exception, EOFError) as error:
+        print("Ошибка при работе с PostgreSQL", error)
 
 
 async def data_add_command(state):
-    async with state.proxy() as data: # открываем словарь, полученный в admin.py
-        cur.execute('INSERT INTO cars VALUES(%s,%s,%s,%s)', tuple(data.values()))
-        base.commit()
+    try:
+        async with state.proxy() as data:  # открываем словарь, полученный в admin.py
+            cur.execute('INSERT INTO cars VALUES(%s,%s,%s,%s)', tuple(data.values()))
+            base.commit()
+    except (Exception, EOFError) as error:
+        print("Ошибка при работе с PostgreSQL", error)
 
 
 async def pga_read(message):
-    cur.execute('SELECT * FROM cars')
-    cars_records = cur.fetchall() # выгружаем данные из базы в виде списка
-    for ret in cars_records:
-        await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена {ret[-1]}')
+    try:
+        cur.execute('SELECT * FROM cars')
+        cars_records = cur.fetchall()  # выгружаем данные из базы в виде списка
+        for ret in cars_records:
+            await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена {ret[-1]}')
+    except (Exception, EOFError) as error:
+        print("Ошибка при работе с PostgreSQL", error)
 
 
 async def pga_read2():
-    cur.execute('SELECT * FROM cars')
-    cars_data = cur.fetchall()
-    return cars_data
+    try:
+        cur.execute('SELECT * FROM cars')
+        cars_data = cur.fetchall()
+        return cars_data
+    except (Exception, EOFError) as error:
+        print("Ошибка при работе с PostgreSQL", error)
 
 
 async def pga_delete_command(data):
-    cur.execute('DELETE FROM cars WHERE name = %s', (data,))
-    base.commit()
+    try:
+        cur.execute('DELETE FROM cars WHERE name = %s', (data,))
+        base.commit()
+    except (Exception, EOFError) as error:
+        print("Ошибка при работе с PostgreSQL", error)
